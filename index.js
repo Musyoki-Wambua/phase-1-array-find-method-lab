a64a3c28bb033c95cbf19180f10889baf594bccc
Musyoki_Wambua <wjosephmusyoki@gmail.com>
2022-11-30 13:54:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function superbowlWin(record) {
+  let result = record.find(record => record.result === "W")
+  //look for a result that is equal to our desired  answer 
+  if(result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
+}
+
+const sadReality =record.find(record => record.result === "N/A")
+
+
+function superbowl (sadReality){
+  return sadReality;
+}
